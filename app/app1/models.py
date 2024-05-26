eb5d666fd36d274db9622ec27da7f47016ec33f8
muhammaduss <muhammad20040130@gmail.com>
2024-05-26 22:04:11 +0300

    adding models to app

--- app/app1/models.py
+++ app/app1/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Users(models.Model):
+    email = models.EmailField(unique=True)
+    verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
